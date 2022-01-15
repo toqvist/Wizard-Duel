fcfa31310fff9ae6b00e3b1a44aa264c17dcb580
toqvist <tobias-ahlqvist@outlook.com>
2022-01-15 12:55:51 +0100

    Created function for determining winner of round. Moved animation to it's own function.

--- wizard-duel.js
+++ wizard-duel.js
@@ -56,14 +56,43 @@ let opponent = {
 
 function duel(event){
 
+    //resetAnimations();
+
     player.natElement = new NaturalElement(document.createElement("i"), event.target.innerText)
     player.zone.append(player.natElement.htmlElement);
 
     opponent.natElement = new NaturalElement(document.createElement("i"), randomElement())
     opponent.zone.append(opponent.natElement.htmlElement);
 
-    //Add class "animate" to all elements that should be animated.
+    
+    animate();
+
+    resolveRound(player.natElement,opponent.natElement)
+    
+}
+//return player or opponent as string
+function resolveRound (playerE, opponentE) {
+
+    
+    console.log(typeof(playerE.beats))
+    console.log(typeof(opponentE.n))
+    //console.log(playerE.beats)
+    // console.log(opponentE.beats)
+
+    if (playerE.beats == opponentE.symbol) {
+        alert("Player Wins!")
+        opponent.shield--;
+    } else if (opponentE.beats == playerE.symbol) {
+        alert("Opponent Wins!")
+        player.shield--;
+    } else {
+        alert("Draw!")
+    }
+
+}
+//Add class "animate" to all elements that should be animated.
     //Animatable elements could be stored in a list in wizard object, and then iterated over in a for loop.
+function animate () {
     player.box.classList.add("animate")
     player.natElement.htmlElement.classList.add("animate");
     player.zone.classList.add("animate");
@@ -71,24 +100,16 @@ function duel(event){
     opponent.box.classList.add("animate")
     opponent.natElement.htmlElement.classList.add("animate");
     opponent.zone.classList.add("animate");
-
-    if (whoWinsRound(player.element, opponent.element) === "Player") {
-        opponent.shield--;
-    } else {
-        player.shield--;
-    }
-    
 }
-//return player or opponent as string
-function whoWinsRound (playerE, opponentE) {
 
-    // if playerE.beats(opponentE) 
-    // 🔥 beats 🌱
-    // 💧 beats 🔥
-    // 🌱 beats 💧
-    // else if opponentE.beats(playerE)
-    //else {return "draw"}
+function resetAnimations() {
+    player.box.classList.remove("animate")
+    player.natElement.htmlElement.classList.aremove("animate");
+    player.zone.classList.remove("animate");
 
+    opponent.box.classList.remove("animate")
+    opponent.natElement.htmlElement.classList.remove("animate");
+    opponent.zone.classList.remove("animate");
 }
 
 
